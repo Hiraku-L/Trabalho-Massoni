f1fdf1474a61da9954c71352fb71cb94f4ee3e70
MateusMorais <mateus.morais.ext@synchro.com.br>
2026-08-05 23:21:56 -0300

    correcoes no cli e arquivo de constantes

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,11 @@
 import argparse
 import csv
 from dataclasses import asdict
-from .utils.constantes import TAMANHOS, RSA, GM, ALGORITMOS
+from .utils.constantes import TAMANHOS, RSA, GM
 from .comparador.comparador import comparar
+from .algoritmos.des import Des
+from .algoritmos.rsa import Rsa
+from .algoritmos.gm import Gm
 
 def main(argv: list[str] | None = None):
     parser = argparse.ArgumentParser()
@@ -26,14 +29,19 @@ def main(argv: list[str] | None = None):
     parser.add_argument(
         "--repetitions",
         help="Quantidade de vezes que cada algoritmo roda para cada tamanho de entrada",
+        type=int,
         default=20
     )
     
     args = parser.parse_args(argv)
 
-    print(args.sizes, args.rsa_bits, args.gm_bits, ALGORITMOS, args.repetitions)
+    algoritmos = [
+        Des(),
+        Gm(args.gm_bits),
+        Rsa(args.rsa_bits)
+    ]
 
-    resultados = comparar(args.sizes, args.rsa_bits, args.gm_bits, ALGORITMOS, args.repetitions)
+    resultados = comparar(args.sizes, algoritmos, args.repetitions)
 
     __salvar_resultado(resultados)
 
--- src/utils/constantes.py
+++ src/utils/constantes.py
@@ -1,8 +1,3 @@
-from ..algoritmos.des import Des
-from ..algoritmos.rsa import Rsa
-from ..algoritmos.gm import Gm
-
 TAMANHOS = [128, 256, 512, 1024]
-RSA = [1024]
-GM = [128]
-ALGORITMOS = [Des(), Rsa(), Gm()]
+RSA = 1024
+GM = 128
