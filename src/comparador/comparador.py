1d9dbcaa92f2fdea18cd728aae8d5da426fe11bd
raul silva vieira <raulzitosilva123@gmail.com>
2026-08-05 23:18:53 -0300

    corrigindo bugs no comparador

--- src/comparador/comparador.py
+++ src/comparador/comparador.py
@@ -7,7 +7,7 @@ def calcula_media_e_desvio_padrao(funcao, repeticoes):
     tempos = []
 
     for i in range(repeticoes):
-        resultado, tempo = medir_tempo(funcao)
+        tempo = medir_tempo(funcao)
         tempos.append(tempo)
 
     media = np.mean(tempos)
@@ -28,19 +28,18 @@ def comparar(tamanhos, algoritmos, repeticoes):
     resultados = []
 
     for algoritmo in algoritmos:
-        chaves = algoritmos.gerar_chaves()
-        tempos_geracao_de_chaves = calcula_media_e_desvio_padrao(algoritmos.gerar_chaves(), repeticoes)
+        chaves = algoritmo.gerar_chaves()
+        tempos_geracao_de_chaves = calcula_media_e_desvio_padrao(lambda: algoritmo.gerar_chaves(), repeticoes)
         media_geracao_de_chaves, desvio_padrao_geracao_de_chaves = tempos_geracao_de_chaves
 
         for tamanho in tamanhos:
             texto = os.urandom(tamanho)
 
             texto_cifrado = algoritmo.cifrar(texto, chaves.chave_publica)
-            tempos_encriptacao = calcula_media_e_desvio_padrao(algoritmo.cifrar(texto, chaves.chave_publica), repeticoes)
+            tempos_encriptacao = calcula_media_e_desvio_padrao(lambda: algoritmo.cifrar(texto, chaves.chave_publica), repeticoes)
             media_encriptacao, desvio_padrao_encriptacao = tempos_encriptacao
 
-            texto_decifrado = algoritmo.decifrar(texto_cifrado, chaves.chave_privada)
-            tempos_decriptacao = calcula_media_e_desvio_padrao(algoritmo.decifrar(texto_cifrado, chaves.chave_privada), repeticoes)
+            tempos_decriptacao = calcula_media_e_desvio_padrao(lambda: algoritmo.decifrar(texto_cifrado, chaves.chave_privada), repeticoes)
             media_decriptacao, desvio_padrao_decriptacao = tempos_decriptacao
 
             tamanho_texto_cifrado = algoritmo.tamanho_texto_cifrado(texto_cifrado, chaves.chave_publica)
